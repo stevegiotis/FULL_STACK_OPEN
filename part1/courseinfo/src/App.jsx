aa0da6369b41802cdd462f1a6bddb68469dae419
stevegiotis <132510262+stevegiotis@users.noreply.github.com>
2023-10-09 09:30:09 +0000

    part1-a

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  //const course = 'Half Stack application development'
+/*const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14 */
+
+  //<Header course={course} />
+  //<Content/>
+  //<Total/>
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+export default App
