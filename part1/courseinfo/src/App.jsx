440a7f7c80cd48a29b4cd823ee543bb9ee9c343a
stevegiotis <132510262+stevegiotis@users.noreply.github.com>
2023-10-09 19:52:19 +0000

    hello

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,73 @@
-const App = () => {
-  //const course = 'Half Stack application development'
-/*const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14 */
+/* eslint-disable react/prop-types */
+// just the header
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  );
+};
+//parts section 
+const Part = (props) => {
+  console.log(props)
+  return(
+    <p>
+      <strong>{props.part.name}</strong>
+      {props.part.exercises} exercises
+    </p>
+  )
+}
+// rendering all the parts into the Content component
+const Content = (props) => {
+  return (
+		<div>
+			<Part part={props.parts[0]} />
+			<Part part={props.parts[1]} />
+			<Part part={props.parts[2]} />
+		</div>
+	);
+};
 
-  //<Header course={course} />
-  //<Content/>
-  //<Total/>
+const Total = (props) => {
+  return(
+    <p>
+      <strong>
+       Exercises num : 
+      </strong>
+      {props.parts.reduce((acc,cur) => 
+        acc + cur.exercises,0 // set 0 as deafult 
+      )
+      }
+    </p>
+    )
+}
+
+const App = () => {
+  const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
   return (
     <div>
-      hi
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
+
 export default App
