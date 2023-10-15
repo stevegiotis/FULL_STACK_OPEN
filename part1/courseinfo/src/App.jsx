51d3403d1f3c81b864d1d52f121131f9bc8d02bc
stevegiotis <132510262+stevegiotis@users.noreply.github.com>
2023-10-15 16:16:50 +0000

    PArt 1- fix courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,73 +1,66 @@
 /* eslint-disable react/prop-types */
-// just the header
+import React from 'react'; // Import React
+
 const Header = (props) => {
-  console.log(props)
   return (
-    <h1>
-      {props.course}
-    </h1>
+    <h1>{props.course.name}</h1> // Access the course name using props
   );
 };
-//parts section 
+
 const Part = (props) => {
-  console.log(props)
-  return(
+  return (
     <p>
-      <strong>{props.part.name}</strong>
-      {props.part.exercises} exercises
+      <strong>{props.part.name}:</strong> {props.part.exercises} exercises
     </p>
-  )
-}
-// rendering all the parts into the Content component
+  );
+};
+
 const Content = (props) => {
   return (
-		<div>
-			<Part part={props.parts[0]} />
-			<Part part={props.parts[1]} />
-			<Part part={props.parts[2]} />
-		</div>
-	);
+    <div>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part} /> // Use a unique 'key' for each element in a map
+      ))}
+    </div>
+  );
 };
 
 const Total = (props) => {
-  return(
+  const totalExercises = props.parts.reduce((acc, cur) => acc + cur.exercises, 0);
+
+  return (
     <p>
-      <strong>
-       Exercises num : 
-      </strong>
-      {props.parts.reduce((acc,cur) => 
-        acc + cur.exercises,0 // set 0 as deafult 
-      )
-      }
+      <strong>Exercises num:</strong> {totalExercises}
     </p>
-    )
-}
+  );
+};
 
 const App = () => {
   const course = {
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-		],
-	};
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
   return (
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
