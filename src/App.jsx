18569b4b9c9801f347b6226b8f2ba8cec407838b
stevegiotis <132510262+stevegiotis@users.noreply.github.com>
2023-12-04 12:33:24 +0000

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,74 @@
 import { useState } from 'react'
 
-function indexOfMax(array) {
-  const max = Math.max(...array)
-  return array.indexOf(max)
+const Statistics = (props) => {
+    const sum = props.good + props.neutral + props.bad
+    if (sum === 0){
+      return (
+        <div>
+          <p>No feedback given</p>
+        </div>
+      )
+    } 
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={props.good} />
+          <StatisticLine text='neutral' value={props.neutral} />
+          <StatisticLine text='bad' value={props.bad} />
+          <StatisticLine text='all' value={sum} />
+          <StatisticLine text='average' value={(props.good * 1 + props.neutral * 0 + props.bad * (-1))/sum} />
+          <StatisticLine text='positive' value={`${parseFloat(props.good / sum) * 100 } %`}/>
+        </tbody>
+      </table>
+    )
 }
 
-function Anecdote({ title, body, votes, onVote }) {
-  const suffix = votes => (votes === 1) ? '' : 's'
+const StatisticLine = (props) => {
   return (
-    <>
-      <h1>{title}</h1>
-      <div>{body}</div>
-      <div>Has {votes} vote{suffix(votes)}</div>
-      <button onClick={onVote}>vote</button>
-    </>
+    <tr>
+      <td>{props.text}</td> 
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-]
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick} >{props.text}</button>
+    </div>
+  )
+}
 
-function App() {
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-  const mostVoted = indexOfMax(votes);
 
-  function nextAnecdote() {
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * anecdotes.length);
-    } while (newIndex === selected);
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    setSelected(newIndex);
+  const handleGoodClick = () => {
+    setGood(good + 1)
   }
 
-  function vote(anecdoteIndex) {
-    setVotes((prevVotes) => {
-      const newVotes = [...prevVotes];
-      newVotes[anecdoteIndex] += 1;
-      return newVotes;
-    });
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
   }
 
   return (
-    <div className="app">
-      <Anecdote
-        title="Anecdote of the day"
-        body={anecdotes[selected]}
-        votes={votes[selected]}
-        onVote={() => vote(selected)}
-      />
-
-      <button onClick={nextAnecdote}>Next Anecdote</button>
-
-      <Anecdote
-        title="Anecdote with most votes"
-        body={anecdotes[mostVoted]}
-        votes={votes[mostVoted]}
-        onVote={() => vote(mostVoted)}
-      />
+    <div>
+        <h1>give feedback</h1>
+        <Button handleClick={handleGoodClick} text='good'/>
+        <Button handleClick={handleNeutralClick} text='neutral'/>
+        <Button handleClick={handleBadClick} text='bad'/>
+        <h1>statistics</h1>
+        <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
